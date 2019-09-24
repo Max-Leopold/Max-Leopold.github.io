e4b144f0318357c0c35ef867eff483257df52ec3
Maximilian Leopold <max.leopold2111@gmail.com>
2019-09-24 22:34:18 +0200

    Add Terminal and Terminal Header

--- js/app.js
+++ js/app.js
@@ -2,27 +2,46 @@ time = 1;
 startComment = false;
 startString = false;
 
-htmlEditor = "/* \n" +
-    "* Hi \n" +
-    "* Im Max. \n" +
-    "* I normally don't do frontend stuff so excuse me if I just write this website while we are talking. \n" +
+
+htmlEditor = "/*\n" +
+    "* Hi\n" +
+    "* Im Max. :waving_hand:\n" +
+    "*\n" +
+    "* I'm a Java Backend Developer and made this wonderful website.\n" +
+    "*\n" +
+    "* Wait what? You don't like it?\n" +
     "* ...\n" +
+    "* Okay...Why's that?\n" +
+    "*\n" +
+    "* You think everything looks horrible?\n" +
     "*/\n" +
     "\n" +
     "body {\n" +
-    " background-color: #1a1c24; \n" +
-    " color: #abb2bf; \n" +
-    " font-family: 'Fira Mono', monospace;\n" +
-    "} \n" +
+    " background-color: #1a1c24;\n" +
+    " color: #abb2bf;\n" +
+    "}\n" +
     "\n" +
-    ".comment { color: #5c6370; } \n" +
-    ".value { color: #56b6c2; } \n" +
-    ".class { color: #de6d77; } \n" +
+    "/*\n" +
+    "* Better now?\n" +
+    "*\n" +
+    "* No?\n" +
+    "*\n" +
+    "* Okay. Then lets start from scratch again.\n" +
+    "* Let me fire up an editor real quick.\n" +
+    "*\n" +
+    "* Give me a second to fire up an editor real quick.\n" +
+    "*/\n" +
+    "\n" +
+    "body {\n" +
+    " font-family: 'Fira Mono', monospace;\n" +
+    " margin= 0px;\n" +
+    " overflow: hidden;\n" +
+    "}\n" +
     "\n" +
     "#editor {\n" +
     " transition: left 500ms;\n" +
     " background-color: #282c34;\n" +
-    " box-sizing: border-box; \n" +
+    " box-sizing: border-box;\n" +
     " overflow: auto;\n" +
     " float: right;\n" +
     " flex: 9 1 0px;\n" +
@@ -38,20 +57,32 @@ htmlEditor = "/* \n" +
     " padding: 5px 0px 0px 10px;\n" +
     "}\n" +
     "\n" +
-    "#terminal {\n" +
+    "#terminalDiv {\n" +
     " background-color: #282c34;\n" +
+    " margin: 0px 5px 5px 5px;\n" +
+    "}\n" +
+    "\n" +
+    "#terminalHeader {\n" +
+    " padding: 2px 0px 2px 10px;\n" +
+    " color: #c426b4;\n" +
+    " margin: 0px 0px 0px 0px;\n" +
+    "}\n" +
+    "\n" +
+    "#terminal {\n" +
     " flex: 2 1 0px;\n" +
     " overflow: auto;\n" +
-    " margin: 0px;\n" +
-    " padding: 5px 0px 0px 10px;\n" +
-    " margin: 0px 5px 5px 5px;\n" +
+    " margin: 0px 0px 0px 0px;\n" +
+    " padding: 2px 0px 5px 10px;\n" +
     " height: 20%;\n" +
     "}\n" +
     "\n" +
     "#fileSystemAndEditorDiv {\n" +
     " flex: 2 1 0px;\n" +
     "}\n" +
-    "\n";
+    "\n" +
+    ".comment { color: #5c6370; }\n" +
+    ".value { color: #56b6c2; }\n" +
+    ".class { color: #de6d77; }";
 
 fileSystem = "/* \n" +
     "* Hi \n" +
@@ -60,45 +91,50 @@ fileSystem = "/* \n" +
     "* ...\n" +
     "*/\n";
 
-terminal = "/* \n" +
-    "* Hi \n" +
-    "* Im Max. \n" +
-    "* I normally don't do frontend stuff so excuse me if I just write this website while we are talking. \n" +
-    "* ...\n" +
-    "*/\n" +
-    "/* \n" +
-    "* Hi \n" +
-    "* Im Max. \n" +
-    "* I normally don't do frontend stuff so excuse me if I just write this website while we are talking. \n" +
-    "* ...\n" +
-    "*/\n"+
-    "* Hi \n" +
-    "* Im Max. \n" +
-    "* I normally don't do frontend stuff so excuse me if I just write this website while we are talking. \n" +
-    "* ...\n" +
-    "*/\n" +
-    "/* \n" +
-    "* Hi \n" +
-    "* Im Max. \n" +
-    "* I normally don't do frontend stuff so excuse me if I just write this website while we are talking. \n" +
-    "* ...\n";
+terminal = "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n" +
+    "max@website ~ %\n";
+
+terminalHeader = "Terminal";
+
+
 
 (async function start() {
-    await writeText(htmlEditor, 0, time, '#editor');
-    await writeText(fileSystem, 0, time, '#fileSystem');
-    await writeText(terminal, 0, time, '#terminal');
+    await writeText(htmlEditor, 0, time, '#editor', 'editor');
+    await writeText(fileSystem, 0, time, '#fileSystem', 'fileSystem');
+    await writeText(terminalHeader, 0, time, '#terminalHeader', 'terminal');
+    await writeText(terminal, 0, time, '#terminal', 'terminal');
     await console.log("Hello World");
 }).call(this);
 
-function writeText(message, index, interval, to) {
+function writeText(message, index, interval, to, scroll) {
     let pre;
     if (index < message.length) {
-        pre = document.getElementById('editor');
+
+        pre = document.getElementById(scroll);
         pre.scrollTop = pre.scrollHeight;
         writeChar(message[index++], to);
         return new Promise((resolve, reject) => {
-            setTimeout( function () {
-                writeText(message, index, interval, to).then(resolve, reject);
+            setTimeout(function () {
+                writeText(message, index, interval, to, scroll).then(resolve, reject);
             }, time);
         });
     }
