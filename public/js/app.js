fe6fa7ee9c379a5603c2ed482ce3099514b5886b
Max Leopold <max.leopold2111@gmail.cm>
2020-01-26 00:16:21 +0100

    Add possibility for html tag generation at runtime

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,14 @@
-time = 1;
-startComment = false;
-startString = false;
-
-
+var time = 1;
+var startComment = false;
+var startString = false;
+var startHtml = false;
+var htmlTagBuffer = '';
+var htmlBeforeTag = '';
 
 (async function start() {
-    const htmlEditorResponse = await fetch('public/text/editor1.txt');
+    const htmlEditorResponse = await fetch('public/text/editor1.css');
     const htmlEditor = await htmlEditorResponse.text();
-    const fileSystemResponse = await fetch('public/text/fileSystem1.txt');
+    const fileSystemResponse = await fetch('public/text/fileSystem1.html');
     const fileSystem = await fileSystemResponse.text();
     const terminalResponse = await fetch('public/text/terminal1.txt');
     const terminal = await terminalResponse.text();
@@ -18,7 +19,6 @@ startString = false;
     await writeText(fileSystem, 0, time, '#fileSystem', 'fileSystem');
     await writeText(terminalHeader, 0, time, '#terminalHeader', 'terminal');
     await writeText(terminal, 0, time, '#terminal', 'terminal');
-    await console.log("Hello World");
 }).call(this);
 
 function writeText(message, index, interval, to, scroll) {
@@ -38,11 +38,12 @@ function writeText(message, index, interval, to, scroll) {
 }
 
 function writeChar(message, to) {
+
     var newHtml = $(to).html();
-    if (message === '/' && startComment === false) {
+    if (message === '/' && startComment === false && startHtml === false) {
         startComment = true;
         newHtml += message;
-    } else if (message === '/' && startComment === true) {
+    } else if (message === '/' && startComment === true && startHtml === false) {
         startComment = false;
         newHtml = $(to).html().replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
     } else if (startComment === true && (message === '.' || message === '?')) {
@@ -52,14 +53,41 @@ function writeChar(message, to) {
         newHtml = $(to).html().replace(/([^:]*)$/, '<em class="value">$1</em>;');
     } else if (message === '{') {
         newHtml = $(to).html().replace(/(.*)$/, '<em class="class">$1</em>{')
-    } else {
+    } else if (message === '$' && startHtml === false) {
+        startHtml = true;
+        htmlBeforeTag = newHtml;
+
+        newHtml += message;
+    } else if (message === '$' && startHtml === true) {
+        startHtml = false;
+
+        var tag = document.createElement('div');
+        tag.innerHTML = htmlTagBuffer;
+
+        console.log(htmlTagBuffer);
+
+
+        document.getElementById(to.slice(1)).innerHTML = htmlBeforeTag;
+        document.getElementById(to.slice(1)).appendChild(tag);
+        console.log(document.getElementById(to.slice(1)).innerHTML);
+        newHtml = document.getElementById(to.slice(1)).innerHTML;
+        console.log(document.getElementById(to.slice(1)).innerHTML);
+        htmlTagBuffer = '';
+    } else if (startHtml) {
+        htmlTagBuffer += message;
+
+        newHtml += message;
+    }
+    else {
         newHtml += message;
         //time = window.innerWidth <= 578 ? 4 : 1;
         //time = 1;
     }
 
+
     $(to).html(newHtml);
     $('#style').append(message);
+
 }
 
 
